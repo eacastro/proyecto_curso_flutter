376c7696594c99842e7cd794791dbec3d13185be
Edisson Castro <edissoncastro.11@hotmail.com>
2020-06-11 18:24:58 -0500

    Creación de HomePage

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:proyecto_platzi_curso_flutter/home_page_screen.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: MyHomePage(),
+        title: 'Curso Dart Proyecto Flutter',
+      ),
+    );
