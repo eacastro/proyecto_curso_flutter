c2012f3b86f169de6b8812d3fd55482a14e4cee6
Edisson Castro <edissoncastro.11@hotmail.com>
2020-06-11 20:02:34 -0500

    Trabajando datos http y Widget FutureBuilder

--- lib/home_page_screen.dart
+++ lib/home_page_screen.dart
@@ -1,31 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:proyecto_platzi_curso_flutter/api/api.dart';
 import 'package:proyecto_platzi_curso_flutter/custom_gradient_button.dart';
+import 'package:proyecto_platzi_curso_flutter/models/photos.dart';
 
 class MyHomePage extends StatelessWidget {
+  final API api = API();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Platzi'),
       ),
-      body: Center(
-        child: CustomGradientButton(
-          text: Text('Colombia'),
-          width: 150.0,
-          height: 40.0,
-          gradientColors: [
-            Colors.yellow,
-            Colors.blue,
-            Colors.red,
-          ],
-          initialPosition: Alignment.centerLeft,
-          finalPosition: Alignment.centerRight,
-          function: () => print(
-            '¡Hola desde mi casa!',
-          ),
-          leadingIcon: Icon(Icons.person),
-          finalIcon: Icon(Icons.chat),
-        ),
+      body: FutureBuilder<List<Photos>>(
+        future: api.getPhotos(),
+        builder: (BuildContext context, AsyncSnapshot<List<Photos>> snapshot) {
+          if (snapshot.hasData &&
+              snapshot.connectionState == ConnectionState.done) {
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30.0,
+                      backgroundImage:
+                          NetworkImage(snapshot.data[index].thumbnailUrl),
+                    ),
+
+                    title: Text(
+                      snapshot.data[index].id.toString(),
+                    ),
+                    subtitle: Text('Descripción del álbum ${snapshot.data[index].albumId}'),
+                  ),
+                );
+              },
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
